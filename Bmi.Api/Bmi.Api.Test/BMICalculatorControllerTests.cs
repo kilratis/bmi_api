65c22d1ba67f4d20b536c8bff3521252fa2ed50e
Jojo <j.mendiola@centrala.kaczmarski.pl>
2024-08-14 00:03:43 +0800

    Fix error on test 1 and add 2 more test cases

--- Bmi.Api/Bmi.Api.Test/BMICalculatorControllerTests.cs
+++ Bmi.Api/Bmi.Api.Test/BMICalculatorControllerTests.cs
@@ -1,6 +1,7 @@
 ﻿using Bmi.Api.Controllers;
 using Bmi.Api.Converters;
 using Bmi.Api.Models.BMICalculatorAPI;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 
 namespace Bmi.Api.Tests
@@ -18,7 +19,6 @@ namespace Bmi.Api.Tests
 
       _controller = new BMICalculatorController(_cmToMeterConverterMock.Object);
     }
-
     [Test]
     public void CalculateBmi_ReturnsCorrectBmiAndCategory()
     {
@@ -36,11 +36,65 @@ namespace Bmi.Api.Tests
 
       // Assert
       Assert.NotNull(result);
-      Assert.IsInstanceOf<BmiResponse>(result.Value);
+      var okResult = result.Result as OkObjectResult;
+      Assert.NotNull(okResult);
+      Assert.IsInstanceOf<BmiResponse>(okResult.Value);
 
-      var response = result.Value;
-      Assert.AreEqual(23.15f, response.Bmi, 0.01f); // Allowing a precision of 2 decimal places
+      var response = okResult.Value as BmiResponse;
+      Assert.AreEqual(23.15f, response.Bmi, 0.01f);
       Assert.AreEqual("Normal weight", response.Category);
+    }
+
+    [Test]
+    public void CalculateBmi_UnderweightCategory()
+    {
+      // Arrange
+      var request = new BmiRequest
+      {
+        Height = 180,  // centimeters
+        Weight = 50    // kilograms
+      };
+      var expectedHeightInMeters = 1.8f;
+      _cmToMeterConverterMock.Setup(x => x.Convert(request.Height)).Returns(expectedHeightInMeters);
+
+      // Act
+      var result = _controller.CalculateBmi(request);
+
+      // Assert
+      Assert.NotNull(result);
+      var okResult = result.Result as OkObjectResult;
+      Assert.NotNull(okResult);
+      Assert.IsInstanceOf<BmiResponse>(okResult.Value);
+
+      var response = okResult.Value as BmiResponse;
+      Assert.AreEqual(15.43f, response.Bmi, 0.01f);
+      Assert.AreEqual("Underweight", response.Category);
+    }
+
+    [Test]
+    public void CalculateBmi_OverweightCategory()
+    {
+      // Arrange
+      var request = new BmiRequest
+      {
+        Height = 160,  // centimeters
+        Weight = 80    // kilograms
+      };
+      var expectedHeightInMeters = 1.6f;
+      _cmToMeterConverterMock.Setup(x => x.Convert(request.Height)).Returns(expectedHeightInMeters);
+
+      // Act
+      var result = _controller.CalculateBmi(request);
+
+      // Assert
+      Assert.NotNull(result);
+      var okResult = result.Result as OkObjectResult;
+      Assert.NotNull(okResult);
+      Assert.IsInstanceOf<BmiResponse>(okResult.Value);
+
+      var response = okResult.Value as BmiResponse;
+      Assert.AreEqual(31.25f, response.Bmi, 0.01f);
+      Assert.AreEqual("Obesity", response.Category);
     }
   }
 }
